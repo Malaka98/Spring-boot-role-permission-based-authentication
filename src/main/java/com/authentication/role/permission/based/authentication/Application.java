5020e81a0fab1c9c8adbc3ce0d26e81eba967850
Malaka98 <malaka.aruna14413@gmail.com>
2022-05-28 23:19:27 +0530

    first commit

---
+++ src/main/java/com/authentication/role/permission/based/authentication/Application.java
@@ -0,0 +1,13 @@
+package com.authentication.role.permission.based.authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/authentication/role/permission/based/authentication/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authentication.role.permission.based.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
